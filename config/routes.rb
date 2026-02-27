71d2b751674083996f1d1c293831d9f9611c5ca3
mtolava <martintolava@gmail.com>
2026-02-27 16:09:55 -0300

    agregamos una ruta para session path que faltaba

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,19 @@
 Rails.application.routes.draw do
+  resource :session, only: [ :new, :create, :destroy ]
+  resources :passwords, param: :token, only: [ :new, :create, :edit, :update ]
+
   root "vehicles#index"
 
   resources :owners do
-    resources :vehicles, only: [:new, :create]
+    resources :vehicles, only: [ :new, :create ]
   end
 
-  resources :vehicles, only: [:index, :show, :edit, :update] do
-    resources :visits, only: [:new, :create]
+  resources :vehicles, only: [ :index, :show, :edit, :update ] do
+    resources :visits, only: [ :new, :create ]
   end
 
-  resources :visits, only: [:index, :show, :edit, :update] do
-    resources :inspections, only: [:new, :create, :edit, :update]
-    resources :diagnoses, only: [:new, :create, :edit, :update]
+  resources :visits, only: [ :index, :show, :edit, :update ] do
+    resources :inspections, only: [ :new, :create, :edit, :update ]
+    resources :diagnoses, only: [ :new, :create, :edit, :update ]
   end
 end
